d24c3637afadd19324331f3f94f84c4b425dc9b6
Aleksandr Efimenko <mail@alexefimenko.com>
2024-01-30 11:53:11 -0500

    add sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ import nextra from 'nextra'
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  siteUrl: process.env.SITE_URL || 'https://blog.alexefimenko.com',
+  generateRobotsTxt: true, // (optional)
 })
 
 export default withNextra()
