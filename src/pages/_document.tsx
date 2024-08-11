cae524c783bb56be386404751c5bd4d2e302a966
Aleksandr Efimenko <mail@alexefimenko.com>
2024-08-11 16:50:47 -0400

    add UMAMI_ID and GOOGLE_ADS_CLIENT_ID to env

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+const umamiId = process.env.NEXT_PUBLIC_UMAMI_ID
+const googleAdsClientId = process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <script async src='https://us.umami.is/script.js' data-website-id='7025f957-6820-49ba-9159-174a144a1619'></script>
+      <Script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${googleAdsClientId}`}
+        strategy='lazyOnload'
+        crossOrigin='anonymous'
+      />
+      <Script async src='https://us.umami.is/script.js' data-website-id={umamiId} />
       <body>
         <Main />
         <NextScript />
