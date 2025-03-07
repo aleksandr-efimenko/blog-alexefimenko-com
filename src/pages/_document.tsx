5c61564944c84c2ea2e33d25a1efb4f63cd6e89a
Aleksandr Efimenko <mail@alexefimenko.com>
2025-03-06 19:35:41 -0500

    add impactId

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
+import { ReferralBanner } from '@/components/ui/referral-banner'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 const umamiId = process.env.NEXT_PUBLIC_UMAMI_ID
 const googleAdsClientId = process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID
+const impactId = process.env.NEXT_PUBLIC_IMPACT_ID
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
       <meta name='google-adsense-account' content={googleAdsClientId}></meta>
+      <meta name='impact-site-verification' value={impactId}></meta>
       <script async src='https://us.umami.is/script.js' data-website-id={umamiId} />
       <script
         async
